82ea70a63178b926eec23d44cb5794ce3a9b42dc
Ben Seager <github@benseager.co.uk>
2017-07-27 11:35:45 +0100

    Added handling of empty responses from the repo

--- __tests__/service.tests.js
+++ __tests__/service.tests.js
@@ -6,20 +6,38 @@ const moment = require('moment');
 
 describe('service', () => {
     let service;
+    let expectedEvent;
+    let mockRepo;
     beforeAll(() => {
         jest.mock('../lib/repository');
+        mockRepo = require('../lib/repository');
+        mockRepo.__createFakes(3);
+
+        expectedEvent = mockRepo.__fakeEvents[0];
+
         service = require('../lib/service');
     });
 
     test('getNextEvent gets the next event if there is one', () => {
         expect.assertions(1);
 
-        return service.getNextEvent().then(response => expect(response).toBeDefined());
+        return service.getNextEvent().then(response => {
+            expect(response).toBe(expectedEvent);
+        });
     });
+
 
     test('getEventDetail returns event detail', () => {
         expect.assertions(1);
 
-        return service.getEventDetail(1).then(response => expect(response).toBe('details'));
+        return service.getEventDetail(1).then(response => expect(response).toBe(expectedEvent.details));
+    });
+
+
+    test('getNextEvent returns an empty object if there are no events', () => {
+        mockRepo.__fakeEvents = [];
+        expect.assertions(1);
+
+        return service.getNextEvent().then(response => expect(response).toMatchObject({}));
     });
 });
